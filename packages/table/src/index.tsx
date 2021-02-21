fa074943d9c1eb0bf789b9574a11babb454d2641
LyhourChhen <ukamilove66@gmail.com>
2021-02-21 17:18:44 +0700

    e

--- packages/table/src/index.tsx
+++ packages/table/src/index.tsx
@@ -5,7 +5,7 @@ interface Props {
   text: string
 }
 // just test only
-// made some change
+// made some change nn
 export const ExampleComponent = ({ text }: Props) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
