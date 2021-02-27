3f91397f76e49cf7bc33dba4a9c4fd1f4f6432b3
lyhourchhen <chhenlyhour@gmail.com>
2021-02-27 23:19:56 +0700

    binding dependency

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,25 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import { AsurRaaDraggableModalProvider } from "@asurraa/sura-ui-modal"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <AsurRaaDraggableModalProvider>    
+      <div >
+      <h2 style={{ textAlign: 'center' }}>SURA-UI EXAMPLE</h2>
     </div>
+    </AsurRaaDraggableModalProvider>
+
   );
 }
 
