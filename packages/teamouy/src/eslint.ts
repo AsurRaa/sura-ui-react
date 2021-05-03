9488568c83a52ab515f5758b86810e68f4888b81
LyhourChhen <ukamilove66@gmail.com>
2021-05-03 10:46:43 +0700

    update @asurraa/sura-ui-teamouy to 1.0.1

--- packages/teamouy/src/eslint.ts
+++ packages/teamouy/src/eslint.ts
@@ -1,6 +1,5 @@
-import * as path from "path";
 import * as fs from "fs";
-import tsEslintConfig from "./tsEslintConfig";
+import * as path from "path";
 
 const parserOptions = {
   ecmaFeatures: {
@@ -44,92 +43,76 @@ if (isTsProject) {
 }
 
 module.exports = {
-  extends: ["eslint-config-airbnb-base", "prettier"].concat(
-    isTsProject
-      ? ["plugin:@typescript-eslint/recommended"]
-      : ["plugin:react/recommended"]
-  ),
-  parser: isTsProject ? "@typescript-eslint/parser" : "@babel/eslint-parser",
-  plugins: ["eslint-comments", "react", "jest", "unicorn", "react-hooks"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:import/errors",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks", "prettier"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
   env: {
     browser: true,
-    node: true,
+    commonjs: true,
     es6: true,
-    mocha: true,
-    jest: true,
-    jasmine: true,
+    node: true,
   },
   rules: {
-    "react/display-name": 0,
-    "react/jsx-props-no-spreading": 0,
-    "react/state-in-constructor": 0,
-    "react/static-property-placement": 0,
-    // Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
-    "react/destructuring-assignment": "off",
-    "react/jsx-filename-extension": "off",
-    "react/no-array-index-key": "warn",
-    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
-    "react-hooks/exhaustive-deps": "warn", // Checks deps of Hooks
-    "react/require-default-props": 0,
-    "react/jsx-fragments": 0,
-    "react/jsx-wrap-multilines": 0,
-    "react/prop-types": 0,
-    "react/forbid-prop-types": 0,
-    "react/sort-comp": 0,
-    "react/react-in-jsx-scope": 0,
-    "react/jsx-one-expression-per-line": 0,
-    "generator-star-spacing": 0,
-    "function-paren-newline": 0,
-    "import/no-unresolved": 0,
-    "import/order": 0,
-    "import/no-named-as-default": 0,
-    "import/no-cycle": 0,
-    "import/prefer-default-export": 0,
-    "import/no-default-export": 0,
-    "import/no-extraneous-dependencies": 0,
-    "import/named": 0,
-    "import/no-named-as-default-member": 0,
-    "import/no-duplicates": 0,
-    "import/no-self-import": 0,
-    "import/extensions": 0,
-    "import/no-useless-path-segments": 0,
-    "jsx-a11y/no-noninteractive-element-interactions": 0,
-    "jsx-a11y/click-events-have-key-events": 0,
-    "jsx-a11y/no-static-element-interactions": 0,
-    "jsx-a11y/anchor-is-valid": 0,
-    "sort-imports": 0,
-    "class-methods-use-this": 0,
-    "no-confusing-arrow": 0,
-    "linebreak-style": 0,
-    // Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
-    "no-prototype-builtins": "off",
-    "unicorn/prevent-abbreviations": "off",
-    // Conflict with prettier
-    "arrow-body-style": 0,
-    "arrow-parens": 0,
-    "object-curly-newline": 0,
-    "implicit-arrow-linebreak": 0,
-    "operator-linebreak": 0,
-    "eslint-comments/no-unlimited-disable": 0,
-    "no-param-reassign": 2,
-    "space-before-function-paren": 0,
-    ...(isTsProject ? tsEslintConfig : {}),
+    indent: ["error", 2],
+    quotes: ["warn", "double"],
+    semi: "off",
+    "no-any": "off",
+    "no-console": "off",
+    "react/display-name": "off",
+    "react-hooks/rules-of-hooks": "warn",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/prop-types": "off",
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "react/jsx-indent": "off",
+    "react/jsx-indent-props": "off",
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "no-empty-interface": "off",
   },
-  settings: {
-    // support import modules from TypeScript files in JavaScript files
+  "settings": {
     "import/resolver": {
-      node: {
-        extensions: isTsProject
-          ? [".js", ".jsx", ".ts", ".tsx", ".d.ts"]
-          : [".js", ".jsx"],
+      "node": {
+        "extensions": [
+          ".js",
+          ".jsx",
+          ".ts",
+          ".tsx",
+          ".*"
+        ]
       },
+      "typescript": {}
     },
     "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx", ".d.ts"],
+      "@typescript-eslint/parser": [
+        ".ts",
+        ".tsx"
+      ]
     },
-    "import/extensions": [".js", ".mjs", ".jsx", ".ts", ".tsx", ".d.ts"],
-    "import/external-module-folders": ["node_modules", "node_modules/@types"],
-    polyfills: ["fetch", "Promise", "URL", "object-assign"],
-  },
-  parserOptions,
+  }
+  ...parserOptions,
 };
