4a604c5fae69c0de8543da56ba7f7d9eee10e457
LyhourChhen <ukamilove66@gmail.com>
2021-05-12 02:02:37 +0700

    add index build

---
+++ index.ts
@@ -0,0 +1,6 @@
+export { Logger, isProductionModeFunc } from "./packages/utilities/src/index";
+export {
+  AsurRaaDraggableModalProvider,
+  AsurRaaModal,
+  AsurRaaModalProps,
+} from "./packages/modal/src/index";
