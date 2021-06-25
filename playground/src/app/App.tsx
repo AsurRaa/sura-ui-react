f7e1fe2ca82fb2e854e14dfbd15d202caf155343
LyhourChhen <ukamilove66@gmail.com>
2021-06-25 21:13:46 +0700

    update

--- playground/src/app/App.tsx
+++ playground/src/app/App.tsx
@@ -1,35 +1,30 @@
-// @ts-nocheck
-import "../styles/App.less";
+import { AsurRaaRichTextEditor } from "@asurraa/sura-ui-rich-text-editor";
 import {
-  AsurRaaModal,
-  AsurRaaDraggableModalProvider,
-  AsurRaaModalProps,
-} from "@asurraa/sura-ui-modal";
-import { Button } from "antd";
-import { FC, useState } from "react";
-import { AsurRaaPaymentModal } from "@asurraa/sura-ui-modal-payment";
-
-const AnotherCustomModal: FC<AsurRaaModalProps> = (props) => {
-  return <AsurRaaModal {...props} />;
-};
+  AsurRaaPaymentModal,
+  AsurRaaPaymentModalProvider,
+} from "@asurraa/sura-ui-modal-payment";
+import "../styles/App.less";
+import { AsurRaaDraggableModalProvider } from "@asurraa/sura-ui-modal";
+import { useState } from "react";
+import Button from "antd/lib/button";
 
 function App() {
-  const [state, setState] = useState<boolean>(false);
-  const [openPaymentModal, setOpenPaymentModal] = useState<boolean>(false);
+  const [openModal, setOpenModal] = useState<boolean>(false);
   return (
     <div className="App">
-      <header className="App-header">Playground</header>
-      <Button onClick={() => setState(true)}>open</Button>
-
+      <AsurRaaRichTextEditor />
       <div>
         <AsurRaaDraggableModalProvider>
-          <AsurRaaPaymentModal visible={true} />
-          <AnotherCustomModal
-            title={"Test Modal"}
-            onCancel={() => setState(false)}
-            visible={state}
-          />
+          <AsurRaaPaymentModalProvider khrExchangeRate={1000}>
+            <AsurRaaPaymentModal
+              visible={openModal}
+              cashToPayDataProps={1000}
+              isPayLoading={false}
+            />
+          </AsurRaaPaymentModalProvider>
         </AsurRaaDraggableModalProvider>
+
+        <Button onClick={() => setOpenModal(!openModal)}>Open Modal</Button>
       </div>
     </div>
   );
--- playground/src/app/AppProvider.tsx
+++ playground/src/app/AppProvider.tsx
@@ -4,9 +4,7 @@ import App from "./App";
 const AppProvider = () => {
   return (
     <Fragment>
-      {/* <AsurRaaUploadProvider> */}
       <App />
-      {/* </AsurRaaUploadProvider> */}
     </Fragment>
   );
 };
