5d2a1ab3c7d795000fcd0f92471edee47cf1b497
LyhourChhen <ukamilove66@gmail.com>
2021-06-26 10:25:21 +0700

    update package

--- playground/src/app/App.tsx
+++ playground/src/app/App.tsx
@@ -1,33 +1,7 @@
-import { AsurRaaRichTextEditor } from "@asurraa/sura-ui-rich-text-editor";
-import {
-  AsurRaaPaymentModal,
-  AsurRaaPaymentModalProvider,
-} from "@asurraa/sura-ui-modal-payment";
 import "../styles/App.less";
-import { AsurRaaDraggableModalProvider } from "@asurraa/sura-ui-modal";
-import { useState } from "react";
-import Button from "antd/lib/button";
-
+import {} from "@asurraa/sura-ui-modal";
 function App() {
-  const [openModal, setOpenModal] = useState<boolean>(false);
-  return (
-    <div className="App">
-      <AsurRaaRichTextEditor />
-      <div>
-        <AsurRaaDraggableModalProvider>
-          <AsurRaaPaymentModalProvider khrExchangeRate={1000}>
-            <AsurRaaPaymentModal
-              visible={openModal}
-              cashToPayDataProps={1000}
-              isPayLoading={false}
-            />
-          </AsurRaaPaymentModalProvider>
-        </AsurRaaDraggableModalProvider>
-
-        <Button onClick={() => setOpenModal(!openModal)}>Open Modal</Button>
-      </div>
-    </div>
-  );
+  return <div className="App">111</div>;
 }
 
 export default App;
