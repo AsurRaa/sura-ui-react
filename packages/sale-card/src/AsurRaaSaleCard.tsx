e1e6b6e775fad2acf813bb35238642bf0c5fc6dd
LyhourChhen <ukamilove66@gmail.com>
2021-06-28 21:49:12 +0700

    update sale card 1.0.7

--- packages/sale-card/src/AsurRaaSaleCard.tsx
+++ packages/sale-card/src/AsurRaaSaleCard.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Card, CardProps, Divider, Typography } from "antd";
 import { FC, Fragment, ReactNode } from "react";
-import { AsurRaaImageViewer } from "@asurraa/sura-ui-image-viewer";
+import {
+  AsurRaaImageViewer,
+  AsurRaaImageViewerProvider,
+} from "@asurraa/sura-ui-image-viewer";
 import styled from "styled-components";
+import { useGetAsurRaaSaleCard } from "./AsurRaaSaleCardProvider";
 
 const Center = styled.div`
   display: flex;
@@ -17,36 +21,42 @@ export interface AsurRaaSaleCardProps {
 }
 
 export const AsurRaaSaleCard: FC<AsurRaaSaleCardProps> = (props) => {
+  const global = useGetAsurRaaSaleCard();
   return (
     <Fragment>
-      <Card
-        style={{
-          boxShadow: "2px 2px 10px 2px rgba(208, 216, 243, 0.6)",
-          cursor: "pointer",
-        }}
-        {...props.antdCardProps}
+      <AsurRaaImageViewerProvider
+        fallbackImage={global?.fallbackImage!}
+        imageUrl={global?.imageUrl!}
       >
-        <Center>
-          <AsurRaaImageViewer isPreview={false} value={props.imageValue} />
-        </Center>
-        <Divider />
-        <Typography.Text style={{ fontSize: 18, fontWeight: "bold" }}>
-          {props?.mainTitle ?? ""}
-        </Typography.Text>
+        <Card
+          style={{
+            boxShadow: "2px 2px 10px 2px rgba(208, 216, 243, 0.6)",
+            cursor: "pointer",
+          }}
+          {...props.antdCardProps}
+        >
+          <Center>
+            <AsurRaaImageViewer isPreview={false} value={props.imageValue} />
+          </Center>
+          <Divider />
+          <Typography.Text style={{ fontSize: 18, fontWeight: "bold" }}>
+            {props?.mainTitle ?? ""}
+          </Typography.Text>
 
-        <div>
-          <div style={{ paddingTop: 5 }}>
-            <Typography.Text style={{ fontWeight: "bold", paddingTop: 10 }}>
-              {props?.mainValueAtLeft ?? ""}
-            </Typography.Text>
+          <div>
+            <div style={{ paddingTop: 5 }}>
+              <Typography.Text style={{ fontWeight: "bold", paddingTop: 10 }}>
+                {props?.mainValueAtLeft ?? ""}
+              </Typography.Text>
+            </div>
+            <div style={{ marginTop: "8px" }}>
+              <Typography.Text style={{ fontWeight: "bold", paddingTop: 10 }}>
+                {props?.mainValueAtRight ?? ""}
+              </Typography.Text>
+            </div>
           </div>
-          <div style={{ marginTop: "8px" }}>
-            <Typography.Text style={{ fontWeight: "bold", paddingTop: 10 }}>
-              {props?.mainValueAtRight ?? ""}
-            </Typography.Text>
-          </div>
-        </div>
-      </Card>
+        </Card>
+      </AsurRaaImageViewerProvider>
     </Fragment>
   );
 };
